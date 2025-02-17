a80d579c0b60764dd707255343a6d1cbb8002065
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-02-17 13:04:19 +0100

    fetchProducts function created

--- src/modules/useProducts.ts
+++ src/modules/useProducts.ts
@@ -13,5 +13,31 @@ interface Product {
 }
 
 export const useProduct = () => {
-   
+   const error = ref<string | null>(null);
+   const loading = ref<boolean>(false);
+   const products = ref<Product[]>([]);
+
+   const fetchProducts = async (): Promise<void> => {
+      loading.value = true;
+
+      try {
+         const response = await fetch('https://ments-restapi.onrender.com/api/products');
+         if (!response.ok) {
+            throw new Error('Failed to fetch products');
+
+         }
+         const data: Product[] = await response.json();
+
+         products.value = data;
+         console.log("Products are fetched: ", products.value);
+      }
+      catch (err) {
+         error.value = (err as Error).message;
+      }
+      finally {
+         loading.value = false;
+      }
+   }
+
+   return { error, loading, products, fetchProducts };
 };
