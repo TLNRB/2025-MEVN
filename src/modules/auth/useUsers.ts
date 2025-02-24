8308848a25f938b4d4c355f71388e870d2153d41
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-02-24 10:56:24 +0100

    fetchToken function added

--- src/modules/auth/useUsers.ts
+++ src/modules/auth/useUsers.ts
@@ -10,4 +10,42 @@ export const useUsers = () => {
    const name = ref<string | null>(null);
    const email = ref<string | null>(null);
    const password = ref<string | null>(null);
+
+   const fetchToken = async (email: string, password: string): Promise<void> => {
+      try {
+         const response = await fetch('https://ments-restapi.onrender.com/api/user/login', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+               'auth-token': localStorage.getItem('lsToken') || ''
+            },
+            body: JSON.stringify({ email, password })
+         })
+
+         if (!response.ok) {
+            const errorResponse = await response.json();
+            console.log(errorResponse.error || 'Invalid credentials');
+
+            throw new Error('Invalid credentials');
+         }
+
+         const authResponse = await response.json();
+
+         token.value = authResponse.data.token;
+         user.value = authResponse.data.user;
+         isLoggedIn.value = true;
+
+         localStorage.setItem('lsToken', authResponse.data.token);
+         localStorage.setItem('userIDToken', authResponse.data.userID);
+
+         console.log('Token:', token.value);
+         console.log('User is logged in: ', authResponse)
+      }
+      catch (err) {
+         error.value = (err as Error).message || 'An error occurred';
+         isLoggedIn.value = false;
+      }
+   }
+
+   return { token, isLoggedIn, error, user, name, email, password, fetchToken };
 };
