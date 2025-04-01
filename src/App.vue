c9a35d3f38262fabc5ca0124d919726acc017540
Norbert Tolnai <tolnainorbi16@gmail.com>
2025-04-01 09:54:56 +0200

    data binding to cart page

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,11 @@ import CartBasket from '@/components/cart/CartBasketView.vue';
 const { logout } = useUsers();
 const isLoggedIn = computed(() => state.isLoggedIn);
 
-const isCartVisible = ref(false);
+const isCartVisible = ref<boolean>(false);
 
-const toggleCart = ():void => {
+const toggleCart = (): void => {
   isCartVisible.value = !isCartVisible.value;
+  console.log("iscartvisible: ", isCartVisible.value);
 };
 </script>
 
@@ -25,7 +26,7 @@ const toggleCart = ():void => {
         <RouterLink to="/products" class="mr-4">Products</RouterLink>
         <RouterLink to="/auth" class="mr-4">Auth</RouterLink>
         <RouterLink v-if="isLoggedIn" to="/admin" class="mr-4">Admin</RouterLink>
-        <butto v-if="isLoggedIn" @click="logout" class="cursor-pointer">Logout</butto>
+        <button v-if="isLoggedIn" @click="logout" class="cursor-pointer">Logout</button>
         <!-- RouterLink to products, Auth, Admin & and logout button -->
 
 
@@ -41,8 +42,8 @@ const toggleCart = ():void => {
 
   <RouterView />
 
-  <!-- CartBasket component -->
-   <CartBasket v-if="isCartVisible" :isVisible="isCartVisible" />
+  <!-- CartBasket component -->   
+   <CartBasket v-if="isCartVisible" @updateIsVisible="toggleCart" :isVisible="isCartVisible" />
 
 </template>
 
--- src/views/cart/CartView.vue
+++ src/views/cart/CartView.vue
@@ -1,31 +1,37 @@
 <script setup lang="ts">
 import { useCart } from '@/modules/cart/useCart';
+
+const { cart, updateQuantity, cartTotal, salesTax, grandTotal, code, checkOutBuy } = useCart();
 </script>
 
 <template>
   <div class="p-4">
     <h2 class="mb-4 text-2xl font-bold">Checkout Section</h2>
-    <p class="pt-5 text-center">Items have been order <br> You have no orders currently</p> <!-- If cart is empty -->
+    <p v-if="cart.length === 0" class="pt-5 text-center">Items have been order <br> You have no orders currently</p>
+    <!-- If cart is empty -->
     <div> <!-- If cart is not empty -->
-      <div class="flex items-center pb-4 mb-4 border-b"> <!-- Loop through the cart items -->
+      <div v-for="item in cart" :key="item._id" class="flex items-center pb-4 mb-4 border-b">
+        <!-- Loop through the cart items -->
         <!-- First Column: Image -->
         <div class="w-1/6">
-          <img  alt="Product Image" class="object-cover w-full h-24 rounded-lg"> <!-- Product image -->
+          <img :src="item.imageURL" alt="Product Image" class="object-cover w-full h-24 rounded-lg">
+          <!-- Product image -->
         </div>
         <!-- Second Column: Title and Description -->
         <div class="w-2/6 px-4">
-          <p class="font-semibold"></p> <!-- Product name -->
-         <!--  <p class="text-gray-500">{{ item.description }}</p> -->
+          <p class="font-semibold">{{ item.name }}</p>
         </div>
         <!-- Third Column: Quantity with + and - -->
         <div class="flex items-center w-1/6">
-          <button  class="px-2 bg-orange-600">-</button> <!-- Decrease quantity -->
-          <span class="mx-2"></span> <!-- Quantity -->
-          <button class="px-2 bg-teal-600">+</button> <!-- Increase quantity -->
+          <button @click="updateQuantity(item._id, item.quantity - 1)" class="px-2 bg-orange-600">-</button>
+          <!-- Decrease quantity -->
+          <span class="mx-2">{{ item.quantity }}</span>
+          <button @click="updateQuantity(item._id, item.quantity + 1)" class="px-2 bg-teal-600">+</button>
+          <!-- Increase quantity -->
         </div>
         <!-- Fourth Column: Total Price -->
         <div class="w-1/6 text-right">
-          <p class="font-semibold">$</p> <!-- Total price of the product with .toFixed() -->
+          <p class="font-semibold">$ {{ (item.price * item.quantity).toFixed(2) }}</p>
         </div>
       </div>
 
@@ -33,28 +39,27 @@ import { useCart } from '@/modules/cart/useCart';
       <div class="pt-4 mt-4">
         <div class="flex justify-between mb-2">
           <p class="font-semibold">Subtotal:</p>
-          <p>$</p> <!-- Total in the cart -->
+          <p>$ {{ cartTotal }}</p>
         </div>
         <div class="flex justify-between mb-2">
           <p class="font-semibold">Sales Tax:</p>
-          <p>$</p>  <!-- Salestax in the cart -->
+          <p>$ {{ salesTax }}</p>
         </div>
         <div class="flex justify-between mb-2">
           <p class="font-semibold">Coupon Code:</p>
-          <input type="text" class="border p-1 pr-2 bg-[#181818] text-right w-28" placeholder="Enter code"> <!-- Coupon code -->
-           </div>
+          <input v-model="code" type="text" class="border p-1 pr-2 bg-[#181818] text-right w-28"
+            placeholder="Enter code">
+        </div>
         <div class="flex justify-between mb-4">
           <p class="font-semibold">Grand Total:</p>
-          <p>$</p>  <!-- Grand total in the cart -->
+          <p>$ {{ grandTotal }}</p>
         </div>
         <div class="flex justify-end">
-          <button class="p-2 text-white bg-orange-600 rounded hover:bg-orange-700" >Buy Now</button> <!-- Checkout button on click -->
+          <button @click="checkOutBuy" class="p-2 text-white bg-orange-600 rounded hover:bg-orange-700">Buy Now</button>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-/* Add your styles here */
-</style>
+<style scoped></style>
